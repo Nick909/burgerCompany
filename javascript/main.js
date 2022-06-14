7dc44e97228cc21fe671f6330d35235b2afd9ef5
Nick909 <nicholas.silva909@outlook.com>
2022-06-14 00:03:08 -0300

    sticky parallax

--- javascript/main.js
+++ javascript/main.js
@@ -7,29 +7,3 @@ function closeMenu() {
   document.body.classList.remove('menu-expanded');
 }
 
-
-$(document).on('scroll', function () {
-  onScroll();
-});
-
-function onScroll() {
-  parallax('#home');
-  parallax('#test');
-}
-
-function parallax(sectionCurrent, topHigh, topPorcent) {
-  const bodyScrollTop = $(document).scrollTop();
-  const screenBodyVieweHight = $(window).height();
-  const screenBodyVieweWidht = $(window).width();
-  
-  const section = $(sectionCurrent);
-  const sectionTop = section.offset().top;
-  const sectionSpan = $(`${sectionCurrent} span`);
-
-  if(topHigh === undefined || topHigh === null) {
-    ((screenBodyVieweWidht < screenBodyVieweHight) && screenBodyVieweHight > 600)? topHigh = 50 : topHigh = 10;
-  }  
-
-  sectionSpan.css('top', `calc((${bodyScrollTop/10}rem + ${0}% + ${topHigh}rem) - (${sectionTop/10}rem)`);
-}
-
---
+++ javascript/stickyParallax.js
@@ -0,0 +1,49 @@
+
+$(document).on('scroll',  () => changesInDimensions());
+
+$(window).on('resize', () => {
+  let topHigh = undefined;
+  if(window.innerHeight < 600) {
+   topHigh = 10;
+  }else {
+   topHigh = 50;
+  }
+
+  changesInDimensions(topHigh);
+})
+
+$(window).on('orientationchange', () => {
+  let topHigh = undefined;
+  if(window.orientation === 90 || window.orientation === -90) {
+   topHigh = 10;
+  }else {
+   topHigh = 50;
+  }
+
+  changesInDimensions(topHigh);
+});
+
+
+function changesInDimensions(n) {
+  parallax('#home', n);
+  parallax('#test', n);
+}
+
+
+function parallax(sectionCurrent, topHigh, topPorcent) {
+  const bodyScrollTop = $(document).scrollTop();
+
+  const screenBodyViewHight = $(window).height();
+  const screenBodyViewWidht = $(window).width();
+  
+  const section = $(sectionCurrent);
+  const sectionTop = section.offset().top;
+  const sectionSpan = $(`${sectionCurrent} span`);
+  
+  if(topHigh === undefined || topHigh === null) {
+    ((screenBodyViewWidht < screenBodyViewHight) || (screenBodyViewHight > 600 ))? 
+    topHigh = 50 : topHigh = 10;
+  }  
+  
+  sectionSpan.css('top', `calc((${bodyScrollTop/10}rem + ${0}% + ${topHigh}rem) - (${sectionTop/10}rem)`);
+}
