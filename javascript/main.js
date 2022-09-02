9b912f3b8545f45efe4447e46a37486c8785ebd9
Nick909 <nicholas.silva909@outlook.com>
2022-09-02 15:01:47 -0300

    por enquanto

--- javascript/main.js
+++ javascript/main.js
@@ -10,11 +10,17 @@ function closeMenu() {
 $(document).on('scroll',  () => changeNav());
 
 function changeNav () {
-  const tam = 44;
+  const tam = 60;
   const num = $(document).scrollTop();
   const nav = $('#navigation')
   const logo = $('#navigation .logo');
   const cart = $('#navigation .btn-group__cart');
+  const link = $('nav#navigation .menu ul li a');
+  const navBefore = $('nav#navigation');
+  
+  console.log(navBefore, 'before');
+
+  const sectionTop = $('#menu').offset().top;
 
   if (num > tam) {
     nav.addClass('navigation--tam');
@@ -24,5 +30,14 @@ function changeNav () {
     nav.removeClass('navigation--tam');
     logo.removeClass('logo--dis');
     cart.removeClass('btn-group__cart--tam');
+  }
+
+
+  if (num >= sectionTop) {
+    link.addClass('link--color');
+    navBefore.addClass('scroll');
+  } else {
+    link.removeClass('link--color');
+    navBefore.removeClass('scroll');
   }
 }
